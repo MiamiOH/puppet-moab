b9850e282f0db9334e5bfe88a26120f8ad97c170
Bradley Koby <kobyb@miamioh.edu>
2019-01-07 10:51:08 -0500

    Add core count fact

--- lib/facter/dmidecode.rb
+++ lib/facter/dmidecode.rb
@@ -15,10 +15,22 @@ Facter.add(:physical_memory) do
   confine osfamily: 'RedHat'
   confine virtual: 'physical'
   setcode do
-    tsm_nodes = {}
+    physical_memory = {}
     if Facter::Util::Resolution.which('/opt/puppetlabs/puppet/bin/dmidecode')
-      tsm_nodes[:total_size] = Facter::Util::Resolution.exec('echo $(( $(( $(/opt/puppetlabs/puppet/bin/dmidecode -t 17| grep -Po \'(?<=Size: )[[:digit:]]+(?= MB)\' | paste -s -d+ -) ))/1024 + $(( $(/opt/puppetlabs/puppet/bin/dmidecode -t 17| grep -Po \'(?<=Size: )[[:digit:]]+(?= GB)\' | paste -s -d+ -) ))))GB') # rubocop:disable LineLength
+      physical_memory[:total_size] = Facter::Util::Resolution.exec('echo $(( $(( $(/opt/puppetlabs/puppet/bin/dmidecode -t 17| grep -Po \'(?<=Size: )[[:digit:]]+(?= MB)\' | paste -s -d+ -) ))/1024 + $(( $(/opt/puppetlabs/puppet/bin/dmidecode -t 17| grep -Po \'(?<=Size: )[[:digit:]]+(?= GB)\' | paste -s -d+ -) ))))GB') # rubocop:disable LineLength
     end
-    tsm_nodes unless tsm_nodes.empty?
+    physical_memory unless physical_memory.empty?
+  end
+end
+
+Facter.add(:processors) do
+  confine osfamily: 'RedHat'
+  confine virtual: 'physical'
+  setcode do
+    processors = {}
+    if Facter::Util::Resolution.which('/opt/puppetlabs/puppet/bin/dmidecode')
+      processors[:cores] = Facter::Util::Resolution.exec('echo $(( $(/opt/puppetlabs/puppet/bin/dmidecode -t 4| grep -Po \'(?<=Core Count: )[[:digit:]]+\' | paste -s -d+ -) ))')
+    end
+    processors unless processors.empty?
   end
 end
